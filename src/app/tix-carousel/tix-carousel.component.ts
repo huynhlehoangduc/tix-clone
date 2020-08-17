bc0eecd0fca7c8ad6b21284d0a4a532c735afd2b
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-08-17 22:29:20 +0700

    Apply tix-carousel

--- src/app/tix-carousel/tix-carousel.component.ts
+++ src/app/tix-carousel/tix-carousel.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class TixCarouselComponent implements OnInit {
 
   slides = [
-    {imgPath: 'https://s3img.vcdn.vn/123phim/2020/08/bi-mat-thien-duong-15972163589211.jpg', youtubeLink: ''},
-    {imgPath: 'https://s3img.vcdn.vn/123phim/2020/08/ca-sau-15972253022491.png', youtubeLink: ''},
-    {imgPath: 'https://s3img.vcdn.vn/123phim/2020/07/du-lich-chet-choc-15961360123636.jpg', youtubeLink: ''},
+    {imgPath: 'https://s3img.vcdn.vn/123phim/2020/08/bi-mat-thien-duong-15972163589211.jpg', youtubeLink: 'https://youtu.be/5IMIdd3iq6A'},
+    {imgPath: 'https://s3img.vcdn.vn/123phim/2020/08/ca-sau-15972253022491.png', youtubeLink: 'https://youtu.be/5IMIdd3iq6A'},
+    {imgPath: 'https://s3img.vcdn.vn/123phim/2020/07/du-lich-chet-choc-15961360123636.jpg', youtubeLink: 'https://youtu.be/5IMIdd3iq6A'},
   ];
 
   slideConfig = { dots : true };
