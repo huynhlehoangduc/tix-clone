a6c14b6f509dff5234327524671b8db3ec11cc4f
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-08-27 00:28:35 +0700

    commit mobile-ads

--- src/app/mobile-ads/mobile-ads.component.ts
+++ src/app/mobile-ads/mobile-ads.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileAdsComponent implements OnInit {
 
+  slideConfig = { dots : true, prevArrow: false, nextArrow: false };
+
   constructor() { }
 
   ngOnInit(): void {
