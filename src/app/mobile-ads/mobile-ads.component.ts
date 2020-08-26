f1446c0989e05199cae6d6ae7813f4a0373ec30f
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-08-26 18:47:37 +0700

    Init mobile-ads

---
+++ src/app/mobile-ads/mobile-ads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-ads',
+  templateUrl: './mobile-ads.component.html',
+  styleUrls: ['./mobile-ads.component.scss']
+})
+export class MobileAdsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
