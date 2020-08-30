070b1b5132de764c31c1856885a38bc199befda9
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-08-30 23:15:38 +0700

    Commit footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  partnerChunks = [
+    [
+      {title: 'CGV', brandLink: 'https://www.cgv.vn/', imgLink: 'https://tix.vn/app/assets/img/icons/cgv.png'},
+      {title: 'BHD', brandLink: 'http://bhdstar.vn', imgLink: 'https://tix.vn/app/assets/img/icons/bhd.png'},
+      {title: 'Galaxy', brandLink: 'http://galaxycine.vn', imgLink: 'https://tix.vn/app/assets/img/icons/galaxycine.png'},
+      {title: 'Cinestar', brandLink: 'http://cinestar.com.vn', imgLink: 'https://tix.vn/app/assets/img/icons/cinestar.png'},
+      {title: 'Lotte Cinema', brandLink: 'http://lottecinemavn.com', imgLink: 'https://s3img.vcdn.vn/123phim/2018/09/404b8c4b80d77732e7426cdb7e24be20.png'},
+    ],
+    [
+      {title: 'CGV', brandLink: 'https://www.cgv.vn/', imgLink: 'https://tix.vn/app/assets/img/icons/cgv.png'},
+      {title: 'BHD', brandLink: 'http://bhdstar.vn', imgLink: 'https://tix.vn/app/assets/img/icons/bhd.png'},
+      {title: 'Galaxy', brandLink: 'http://galaxycine.vn', imgLink: 'https://tix.vn/app/assets/img/icons/galaxycine.png'},
+      {title: 'Cinestar', brandLink: 'http://cinestar.com.vn', imgLink: 'https://tix.vn/app/assets/img/icons/cinestar.png'},
+      {title: 'Lotte Cinema', brandLink: 'http://lottecinemavn.com', imgLink: 'https://s3img.vcdn.vn/123phim/2018/09/404b8c4b80d77732e7426cdb7e24be20.png'},
+    ],
+    [
+      {title: 'CGV', brandLink: 'https://www.cgv.vn/', imgLink: 'https://tix.vn/app/assets/img/icons/cgv.png'},
+      {title: 'BHD', brandLink: 'http://bhdstar.vn', imgLink: 'https://tix.vn/app/assets/img/icons/bhd.png'},
+      {title: 'Galaxy', brandLink: 'http://galaxycine.vn', imgLink: 'https://tix.vn/app/assets/img/icons/galaxycine.png'},
+      {title: 'Cinestar', brandLink: 'http://cinestar.com.vn', imgLink: 'https://tix.vn/app/assets/img/icons/cinestar.png'},
+      {title: 'Lotte Cinema', brandLink: 'http://lottecinemavn.com', imgLink: 'https://s3img.vcdn.vn/123phim/2018/09/404b8c4b80d77732e7426cdb7e24be20.png'},
+    ],
+    [
+      {title: 'CGV', brandLink: 'https://www.cgv.vn/', imgLink: 'https://tix.vn/app/assets/img/icons/cgv.png'},
+      {title: 'BHD', brandLink: 'http://bhdstar.vn', imgLink: 'https://tix.vn/app/assets/img/icons/bhd.png'},
+      {title: 'Galaxy', brandLink: 'http://galaxycine.vn', imgLink: 'https://tix.vn/app/assets/img/icons/galaxycine.png'},
+      {title: 'Cinestar', brandLink: 'http://cinestar.com.vn', imgLink: 'https://tix.vn/app/assets/img/icons/cinestar.png'},
+      {title: 'Lotte Cinema', brandLink: 'http://lottecinemavn.com', imgLink: 'https://s3img.vcdn.vn/123phim/2018/09/404b8c4b80d77732e7426cdb7e24be20.png'},
+    ],
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
