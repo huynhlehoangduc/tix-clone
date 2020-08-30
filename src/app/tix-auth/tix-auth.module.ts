54363b01744f32c7a57824442e507057e14eef86
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-08-31 00:23:48 +0700

    Add and build login

--- src/app/tix-auth/tix-auth.module.ts
+++ src/app/tix-auth/tix-auth.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TixAuthComponent } from './tix-auth.component';
 import { SigninComponent } from './signin/signin.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [TixAuthComponent, SigninComponent],
+  declarations: [TixAuthComponent, SigninComponent, LoginComponent],
   imports: [
     CommonModule
   ],
