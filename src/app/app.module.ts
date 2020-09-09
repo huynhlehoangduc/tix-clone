3281b780c6c4766d473ed9df9ad7401ff55d7b4e
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-09-09 18:38:12 +0700

    Reupdate layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 // Import your library
-// import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
-import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-// import { MobileAdsComponent } from './mobile-ads/mobile-ads.component';
-// import { MovieInfoComponent } from './movie-info/movie-info.component';
-// import { NewsComponent } from './news/news.component';
-// import { TixCarouselComponent } from './tix-carousel/tix-carousel.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomeComponent} from './home.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {NewsComponent} from './news/news.component';
+import {TixCarouselComponent} from './tix-carousel/tix-carousel.component';
+import {SlickCarouselModule} from 'ngx-slick-carousel';
+import {MobileAdsComponent} from './mobile-ads/mobile-ads.component';
+import {MovieInfoComponent} from './movie-info/movie-info.component';
 
 const routes: Routes = [
   {
@@ -22,12 +23,13 @@ const routes: Routes = [
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    // MobileAdsComponent,
-    // MovieInfoComponent,
-    // NewsComponent,
-    // TixCarouselComponent,
+    MobileAdsComponent,
+    MovieInfoComponent,
+    NewsComponent,
+    TixCarouselComponent,
   ],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [SlickCarouselModule, CommonModule, RouterModule.forChild(routes)],
   exports: [HomeComponent],
 })
-export class HomeModule {}
+export class HomeModule {
+}
