03c65bf8fcfbebfce7014f4b7c0a753eecfbf586
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-09-11 22:28:30 +0700

    Refactor layout

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
 import {RouterModule, Routes} from '@angular/router';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
-import {NewsComponent} from './news/news.component';
-import {TixCarouselComponent} from './tix-carousel/tix-carousel.component';
+import {HeaderComponent} from './@header/header.component';
+import {FooterComponent} from './@footer/footer.component';
+import {NewsComponent} from './@news/news.component';
+import {TixCarouselComponent} from './@tix-carousel/tix-carousel.component';
 import {SlickCarouselModule} from 'ngx-slick-carousel';
-import {MobileAdsComponent} from './mobile-ads/mobile-ads.component';
-import {MovieInfoComponent} from './movie-info/movie-info.component';
+import {MobileAdsComponent} from './@mobile-ads/mobile-ads.component';
+import {MovieInfoComponent} from './@movie-info/movie-info.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
